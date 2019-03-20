5dc770682ec33de34b78bce4e317480e38cf3e96
kalasnokov <gustaffagerstrom@gmail.com>
2019-03-20 17:10:34 +0100

    Shader loading and module creation

---
+++ Vulkan/Vulkan/Shaders/compiler.bat
@@ -0,0 +1,3 @@
+E:/Program/Vulkan/Bin32/glslangValidator.exe -V vertex.vert
+E:/Program/Vulkan/Bin32/glslangValidator.exe -V fragment.frag
+pause
--- Vulkan/Vulkan/Vulkan.cpp
+++ Vulkan/Vulkan/Vulkan.cpp
@@ -17,6 +17,7 @@
 #include <vector>
 #include <optional>
 #include <set>
+#include <fstream>
 
 #include <filesystem>
 #include "threadPool.hpp"
@@ -171,7 +172,42 @@ private:
 	}
 
 	void createGraphicsPipeline() {
+		auto vertShaderCode = readFile("../../Vulkan/Shaders/vert.spv");
+		auto fragShaderCode = readFile("../../Vulkan/Shaders/frag.spv");
 
+		VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+		VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+		VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+		vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+		vertShaderStageInfo.module = vertShaderModule;
+		vertShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+		fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+		fragShaderStageInfo.module = fragShaderModule;
+		fragShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+		vkDestroyShaderModule(device, fragShaderModule, nullptr);
+		vkDestroyShaderModule(device, vertShaderModule, nullptr);
+	}
+
+	VkShaderModule createShaderModule(const std::vector<char>& code) {
+		VkShaderModuleCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		VkShaderModule shaderModule;
+		if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create shader module!");
+		}
+
+		return shaderModule;
 	}
 
 	void createImageViews() {
@@ -574,6 +610,27 @@ private:
 
 			return actualExtent;
 		}
+	}
+
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("Failed to open file!");
+		}
+		else {
+			std::cout << "Loaded " << filename << " successfully.\n";
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+
+		return buffer;
 	}
 
 	//GLFW variabels
