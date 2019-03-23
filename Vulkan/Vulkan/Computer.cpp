2f95a134c45cf069ad685b0688c004866341ff9b
kalasnokov <gustaffagerstrom@gmail.com>
2019-03-23 18:18:31 +0100

    Setup bindings for computeMesh

--- Vulkan/Vulkan/Computer.cpp
+++ Vulkan/Vulkan/Computer.cpp
@@ -18,20 +18,25 @@ void Computer::commandBufferSetup() {
 }
 
 void Computer::descriptorSetup() {
-	VkDescriptorSetLayoutBinding descriptorSetLayoutBinding = {};
-	descriptorSetLayoutBinding.binding = 0;
-	descriptorSetLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-	descriptorSetLayoutBinding.descriptorCount = 1;
-	descriptorSetLayoutBinding.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+	VkDescriptorSetLayoutBinding descriptorSetLayoutBinding[2];
+	descriptorSetLayoutBinding[0].binding = 0;
+	descriptorSetLayoutBinding[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	descriptorSetLayoutBinding[0].descriptorCount = 1;
+	descriptorSetLayoutBinding[0].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+
+	descriptorSetLayoutBinding[1].binding = 1;
+	descriptorSetLayoutBinding[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	descriptorSetLayoutBinding[1].descriptorCount = 1;
+	descriptorSetLayoutBinding[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
 
 	VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo = {};
 	descriptorSetLayoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-	descriptorSetLayoutCreateInfo.bindingCount = 1;
-	descriptorSetLayoutCreateInfo.pBindings = &descriptorSetLayoutBinding;
+	descriptorSetLayoutCreateInfo.bindingCount = 2;
+	descriptorSetLayoutCreateInfo.pBindings = descriptorSetLayoutBinding;
 
 	// Create the descriptor set layout. 
 	if (vkCreateDescriptorSetLayout(*device, &descriptorSetLayoutCreateInfo, NULL, &descriptorSetLayout) != VK_SUCCESS) {
-		throw std::runtime_error("COMPUTE ERROR: FAILED TO SETUP DESCRIPTOR!");
+		throw std::runtime_error("COMPUTER ERROR: FAILED TO SETUP DESCRIPTOR!");
 	}
 }
 
@@ -48,7 +53,7 @@ void Computer::pipelineSetup() {
 	pipelineLayoutCreateInfo.pSetLayouts = &descriptorSetLayout;
 
 	if (vkCreatePipelineLayout(*device, &pipelineLayoutCreateInfo, NULL, &pipelineLayout) != VK_SUCCESS) {
-		throw std::runtime_error("COMPUTE ERROR: FAILED TO SETUP PIPELINE LAYOUT!");
+		throw std::runtime_error("COMPUTER ERROR: FAILED TO SETUP PIPELINE LAYOUT!");
 	}
 }
 
