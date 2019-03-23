0667547342e3b5f7986a4bb0b6462f4874905121
Patrik Sjölund <patrik__96@hotmail.com>
2019-03-23 13:45:36 +0100

    Compute n shit

--- Vulkan/Vulkan/terrainGenerator.cpp
+++ Vulkan/Vulkan/terrainGenerator.cpp
@@ -1,16 +1,19 @@
 #include "pch.h"
 #include "terrainGenerator.hpp"
-
+#include <limits>
+#include <iostream>
 
 
 void terrainGenerator::setUp(dataObjects * dataObjects)
 {
+	srand((unsigned)time_t(NULL));
+	seed = std::numeric_limits<float>::min() + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (std::numeric_limits<float>::max() - std::numeric_limits<float>::min())));
 	dataObjectptr = dataObjects;
 	auto physicalDevice = dataObjects->physicalDevice;
 	auto instance = dataObjects->instance;
+	auto device = dataObjects->device;
 
-	VkDeviceCreateInfo createInfo = {};
-	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
 }
 
 terrainGenerator::terrainGenerator() {}
@@ -18,5 +21,11 @@ terrainGenerator::terrainGenerator() {}
 
 terrainGenerator::~terrainGenerator()
 {
-	//Unmap memory
+	compute.storageBuffer.destroy();
+	compute.uniformBuffer.destroy();
+	vkDestroyPipelineLayout(dataObjectptr->device, compute.pipelineLayout, nullptr);
+	vkDestroyDescriptorSetLayout(dataObjectptr->device, compute.descriptorSetLayout, nullptr);
+	vkDestroyPipeline(dataObjectptr->device, compute.pipeline, nullptr);
+	vkDestroyFence(dataObjectptr->device, compute.fence, nullptr);
+	vkDestroyCommandPool(dataObjectptr->device, compute.commandPool, nullptr);
 }
